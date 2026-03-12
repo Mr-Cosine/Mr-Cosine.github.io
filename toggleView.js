f6165fdaa0b0374fa52c8c202f9d924bb1cc0720
Jiaqi Sun <a95sun@uwaterloo.ca>
2026-03-12 16:16:59 -0400

    Updated contents in bolt action rifle section

---
+++ toggleView.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const viewer = document.getElementById("rifle");
+    const toggleBtn = document.getElementById("toggleViewBtn");
+
+    let exploded = false;
+
+    toggleBtn.addEventListener("click", () => {
+        exploded = !exploded;
+
+        viewer.src = exploded
+            ? "asset/models/rifle_explode.glb"
+            : "asset/models/rifle.glb";
+
+        toggleBtn.textContent = exploded
+            ? "Normal View"
+            : "Exploded View";
+    });
+});
