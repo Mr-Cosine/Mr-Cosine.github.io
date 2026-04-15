38c6efa874f5a0275702b316c83e9cf38d07ced1
Jiaqi Sun <jiaqisun.100@gmail.com>
2026-04-15 14:15:52 -0400

    updated lookings of toggleViewBtn

--- toggleView.js
+++ toggleView.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const viewer = document.getElementById("rifle");
     const toggleBtn = document.getElementById("toggleViewBtn");
+    const icon = document.querySelector(".Btnicon");
 
     let exploded = false;
 
     toggleBtn.addEventListener("click", () => {
         exploded = !exploded;
 
-        viewer.src = exploded
-            ? "asset/models/rifle_explode.glb"
+        viewer.src = exploded?
+            "asset/models/rifle_explode.glb"
             : "asset/models/rifle.glb";
 
-        toggleBtn.textContent = exploded
-            ? "Normal"
-            : "Exploded";
+        icon.src = exploded? 
+            'asset/images/exploded view icon.png'
+            : 'asset/images/normal view icon.png';
+        
+        toggleBtn.title = exploded?
+            "Change to Normal View"
+            : "Change to Exploded View"
     });
 });
